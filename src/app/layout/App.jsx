883272ffaa0b0c58bf855535a1ebf37ba9fb6b2d
azizhish <azizhish@gmail.com>
2022-08-23 10:50:43 +0400

    end of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventsDashboard/EventDashboard";
+import NavBar from "../../features/events/nav/NavBar";
 
-
+// Can only return one element, need to have one root div / Fragment / empty tags
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
+
   return (
-    <div className="App">
-      <h1>Re-vents</h1>
-    </div>
+    <>
+      <NavBar setFormOpen={setFormOpen}></NavBar>
+      <Container className="main">
+        <EventDashboard formOpen = {formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </>
   );
 }
 
---
+++ src/features/events/eventForm/EventForm.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, Form, Header, Segment } from "semantic-ui-react";
+
+export default function EventForm({setFormOpen}) {
+    return (
+        <Segment clearing>
+        <Form>
+            <Header content='Create new event' />
+            <Form.Field>
+                <input type='text' placeholder='Event Title' />
+            </Form.Field>
+            <Form.Field>
+                <input type='text' placeholder='Description' />
+            </Form.Field>
+            <Form.Field>
+                <input type='text' placeholder='City' />
+            </Form.Field>
+            <Form.Field>
+                <input type='text' placeholder='Venue' />
+            </Form.Field>
+            <Form.Field>
+                <input type='date' placeholder='Date' />
+            </Form.Field>
+            <Button type="submit" floated="right" positive content="Submit"/>
+            <Button type="submit" floated="right" content="Cancel" onClick={() => setFormOpen(false)}/>
+        </Form>
+        </Segment>
+    )
+}
