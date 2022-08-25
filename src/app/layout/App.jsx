5a2519f2856db0e387ec3aa732ce444560f66122
azizhish <azizhish@gmail.com>
2022-08-25 12:09:23 +0400

    end of section5

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,36 +1,31 @@
-import React, { useState } from "react";
+import React, {  } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
+import EventDetailedPage from "../../features/events/eventDetailed/EventDetailedPage";
+import EventForm from "../../features/events/eventForm/EventForm";
 import EventDashboard from "../../features/events/eventsDashboard/EventDashboard";
-import NavBar from "../../features/events/nav/NavBar";
+import NavBar from "../../features/nav/NavBar";
+import HomePage from "../../features/home/HomePage";
 
 // Can only return one element, need to have one root div / Fragment / empty tags
 function App() {
-  const [formOpen, setFormOpen] = useState(false);
-
-  const [selectedEvent, setSelectedEvent] = useState(null);
-
-  function handleSelectEvent(event) {
-    setSelectedEvent(event);
-    setFormOpen(true);
-  }
-
-  function handleCreateFormOpen() {
-    setSelectedEvent(null);
-    setFormOpen(true);
-  }
 
   return (
-    <>
-      <NavBar setFormOpen={handleCreateFormOpen} />
+    <BrowserRouter>
+    <Route exact path="/" component={HomePage} />
+    <Route path={'/(.+)'} render={() => (
+      <><NavBar  />
       <Container className="main">
-        <EventDashboard
-          formOpen={formOpen}
-          setFormOpen={setFormOpen}
-          selectEvent={handleSelectEvent}
-          selectedEvent={selectedEvent}
-        />
-      </Container>
-    </>
+          
+          <Route exact path="/events" component={EventDashboard} />
+          <Route path="/events/:id" component={EventDetailedPage} />
+          <Route path={["/createEvent", ]} component={EventForm} />
+      </Container></>
+      
+    )} />
+      
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const rootEl = ReactDOM.createRoot(document.getElementById('root'));
 function render(){
-  ReactDOM.render(<App />, rootEl);
+  ReactDOM.render(
+  <App />
+  
+  , rootEl);
 }
 
 if (module.hot) {
